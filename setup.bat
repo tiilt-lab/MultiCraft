6f6e8c5eab057a6f14e76b4b9957752c0caae704
Kevin Mendoza Tudares <kevinamendoza@users.noreply.github.com>
2020-05-02 22:27:15 -0500

    fixed conda environment name in setup.bat

--- setup.bat
+++ setup.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Please set up Miniconda to use this program. See manual_setup.md for more details. Use Ctrl+C to cancel.
 timeout 10
-call conda create --name tiilt-multi-copy python=3.6
-call conda activate tiilt-multi-copy
+call conda create --name tiilt-multi python=3.6
+call conda activate tiilt-multi
 call conda install -c anaconda pyaudio
 call pip install ibm_watson
 call pip install spacy
