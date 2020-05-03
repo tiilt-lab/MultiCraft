5a313e90fea24410a51fe2740990ef42382b19ed
Kevin Mendoza Tudares <kevinamendoza@users.noreply.github.com>
2020-05-02 22:24:48 -0500

    updated setup.bat

--- setup.bat
+++ setup.bat
@@ -1,10 +1,8 @@
 @echo off
-echo Please set up Miniconda to use this program. See manual_setup.md for more details.
+echo Please set up Miniconda to use this program. See manual_setup.md for more details. Use Ctrl+C to cancel.
 timeout 10
-call conda init cmd.exe && conda init powershell
-call cmd
-call conda create --name tiilt-multi python=3.6
-call conda activate tiilt-multi
+call conda create --name tiilt-multi-copy python=3.6
+call conda activate tiilt-multi-copy
 call conda install -c anaconda pyaudio
 call pip install ibm_watson
 call pip install spacy
