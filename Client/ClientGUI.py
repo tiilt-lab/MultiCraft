5461379cea9f960e7985742ecca4a9277df37c55
mczxy <mitchellzhen@gmail.com>
2020-11-01 16:43:01 -0600

    added voice commands to ClientGUI

--- Client/ClientGUI.py
+++ Client/ClientGUI.py
@@ -4,25 +4,25 @@ import tkinter as tk
 import urllib.request
 import uuid
 from queue import Queue, Full
-from threading import Thread
+from threading import Event, Thread
 
-# import pyaudio
-# from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-# from ibm_watson import SpeechToTextV1
-# from ibm_watson.websocket import RecognizeCallback, AudioSource
+import pyaudio
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import SpeechToTextV1
+from ibm_watson.websocket import RecognizeCallback, AudioSource
 
 from EyeTracker import process_eye_tracking
 
-# # PyAudio Configuration
-# CHUNK = 1024
-# BUF_MAX_SIZE = CHUNK * 10
-# q = Queue(maxsize=int(round(BUF_MAX_SIZE / CHUNK)))
-# FORMAT = pyaudio.paInt16
-# CHANNELS = 1
-# RATE = 44100
+# PyAudio Configuration
+CHUNK = 1024
+BUF_MAX_SIZE = CHUNK * 10
+q = Queue(maxsize=int(round(BUF_MAX_SIZE / CHUNK)))
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
 
-# # Create an instance of AudioSource
-# audio_source = AudioSource(q, True, True)
+# Create an instance of AudioSource
+audio_source = AudioSource(q, True, True)
 
 def get_uuid(mc_username):
     global CLIENT_NAME
@@ -46,70 +46,71 @@ def connect_to_server(server_ip):
     except:
         return f'Unable to connect: IP may be incorrect'
 
-# def connect_to_voice():
-#     global SPEECH_TO_TEXT
-#     with open('./client_credentials.json'):
-#         credentials = json.load(f)
-#         API_KEY = credentials['api_key']
-#         SERVICE_URL = credentials['service_url']
-#         CUSTOMIZATION_ID = credentials['customization_id']
-#     authenticator = IAMAuthenticator(API_KEY)
-#     SPEECH_TO_TEXT = SpeechToTextV1(authenticator=authenticator)
-#     SPEECH_TO_TEXT.set_service_url(SERVICE_URL)
+def connect_to_voice():
+    global SPEECH_TO_TEXT, CUSTOMIZATION_ID
+    with open('./client_credentials.json') as f:
+        credentials = json.load(f)
+        API_KEY = credentials['api_key']
+        SERVICE_URL = credentials['service_url']
+        CUSTOMIZATION_ID = credentials['customization_id']
+    authenticator = IAMAuthenticator(API_KEY)
+    SPEECH_TO_TEXT = SpeechToTextV1(authenticator=authenticator)
+    SPEECH_TO_TEXT.set_service_url(SERVICE_URL)
 
-# # Define callback for the Speech to Text service
-# class MyRecognizeCallback(RecognizeCallback):
-#     def __init__(self):
-#         RecognizeCallback.__init__(self)
+# Define callback for the Speech to Text service
+class MyRecognizeCallback(RecognizeCallback):
+    def __init__(self):
+        RecognizeCallback.__init__(self)
 
-#     def on_transcription(self, transcript):
-#         print(transcript)
+    def on_transcription(self, transcript):
+        print(transcript)
 
-#     def on_connected(self):
-#         print('Connection was successful')
+    def on_connected(self):
+        print('Connection was successful')
 
-#     def on_error(self, error):
-#         print(f'Error received: {error}')
+    def on_error(self, error):
+        print(f'Error received: {error}')
 
-#     def on_inactivity_timeout(self, error):
-#         print(f'Inactivity timeout: {error}')
+    def on_inactivity_timeout(self, error):
+        print(f'Inactivity timeout: {error}')
 
-#     def on_listening(self):
-#         print('Service is listening\nEnter CTRL+C to end recording...')
+    def on_listening(self):
+        print('Service is listening\nEnter CTRL+C to end recording...')
 
-#     def on_hypothesis(self, hypothesis):
-#         pass
+    def on_hypothesis(self, hypothesis):
+        pass
 
-#     def on_data(self, data):
-#         # Once received a command, print and send the command string to the server
-#         if(data['results'][0]['final']):
-#             transcript = data['results'][0]['alternatives'][0]['transcript'].lower()
-#             print(transcript)
-#             process_eye_tracking(transcript)
-#             CLIENT_SOCKET.send((CLIENT_NAME + " " + result).encode())
+    def on_data(self, data):
+        # Once received a command, print and send the command string to the server
+        if(data['results'][0]['final']):
+            transcript = data['results'][0]['alternatives'][0]['transcript'].lower()
+            print(transcript)
+            frame5.voice_command(transcript)
+            process_eye_tracking(transcript)
+            CLIENT_SOCKET.send((CLIENT_NAME + " " + transcript).encode())
 
-#     def on_close(self):
-#         client_socket.close()
-#         print('Connection closed')
+    def on_close(self):
+        client_socket.close()
+        print('Connection closed')
 
-# # Initiate the recognize service and pass in the AudioSource
-# def recognize_using_websocket(*args):
-#     mycallback = MyRecognizeCallback()
+# Initiate the recognize service and pass in the AudioSource
+def recognize_using_websocket(*args):
+    mycallback = MyRecognizeCallback()
 
-#     SPEECH_TO_TEXT.recognize_using_websocket(audio=audio_source,
-#                                              content_type='audio/l16; rate=44100',
-#                                              recognize_callback=mycallback,
-#                                              language_customization_id=CUSTOMIZATION_ID,
-#                                              customization_weight=0.9,
-#                                              interim_results=True)
+    SPEECH_TO_TEXT.recognize_using_websocket(audio=audio_source,
+                                             content_type='audio/l16; rate=44100',
+                                             recognize_callback=mycallback,
+                                             language_customization_id=CUSTOMIZATION_ID,
+                                             customization_weight=0.9,
+                                             interim_results=True)
 
-# # Define callback for PyAudio to store the recording in queue
-# def pyaudio_callback(in_data, frame_count, time_info, status):
-#     try:
-#         q.put(in_data)
-#     except Full:
-#         pass # discard
-#     return (None, pyaudio.paContinue)
+# Define callback for PyAudio to store the recording in queue
+def pyaudio_callback(in_data, frame_count, time_info, status):
+    try:
+        q.put(in_data)
+    except Full:
+        pass # discard
+    return (None, pyaudio.paContinue)
 
 class Frame1():
     def __init__(self, parent):
@@ -193,6 +194,7 @@ class Frame3():
         self.close_frame()
         voice_label = tk.Label(text='Using voice commands')
         voice_label.pack()
+        global frame5
         frame5 = Frame5(root)
 
     def close_frame(self):
@@ -206,7 +208,7 @@ class Frame4():
         self.entry = tk.Entry(master=self.frame)
         self.button = tk.Button(master=self.frame, text='Send', command=self.send_command)
         self.counter = 0
-        self.msg_label = tk.Label(master=self.frame, text='Ready')
+        self.msg_label = tk.Label(master=self.frame, text=f'[{self.counter}] Ready')
         self.quit_btn = tk.Button(master=self.frame, text='Quit', command=parent.destroy)
         self.label.pack()
         self.entry.pack()
@@ -224,6 +226,54 @@ class Frame4():
 
     def close_frame(self):
         self.frame.destroy()
+
+class Frame5():
+    def __init__(self, parent):
+        self.parent = parent
+        self.frame = tk.Frame(self.parent)
+
+        connect_to_voice()
+        self.audio = pyaudio.PyAudio()
+        self.stream = self.audio.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            frames_per_buffer=CHUNK,
+            stream_callback=pyaudio_callback,
+            start=False
+        )
+        self.stream.start_stream()
+
+        self.recognize_thread = Thread(target=recognize_using_websocket, args=())
+        self.recognize_thread.start()
+        
+        self.prompt_lbl = tk.Label(master=self.frame, text='Transcript:')
+        self.transcript_lbl = tk.Label(master=self.frame, text='__________')
+        self.counter = 0
+        self.msg_label = tk.Label(master=self.frame, text=f'[{self.counter}] Ready')
+        self.stop_button = tk.Button(master=self.frame, text='Stop Recording', command=self.stop)
+        self.quit_btn = tk.Button(master=self.frame, text='Quit', command=parent.destroy)
+        self.prompt_lbl.pack()
+        self.transcript_lbl.pack()
+        self.msg_label.pack()
+        self.stop_button.pack()
+        self.quit_btn.pack()
+        self.frame.pack()
+
+    def voice_command(self, transcript):
+        self.transcript_lbl.config(text=transcript)
+        self.counter += 1
+        self.msg_label.config(text=f'[{self.counter}] Command sent')
+    
+    def stop(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio.terminate()
+        audio_source.completed_recording()
+        self.msg_label.config(text='Recording stopped')
+    
+
 
 root = tk.Tk()
 frame1 = Frame1(root)
